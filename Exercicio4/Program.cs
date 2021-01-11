3f6aa9989dfce4bf818dc6a1891e5c4f967a89ea
Davez99 <69636802+Davez99@users.noreply.github.com>
2021-01-11 13:27:20 -0300

    tratado exceção com try catch

--- Exercicio4/Program.cs
+++ Exercicio4/Program.cs
@@ -1,35 +1,41 @@
-﻿using System;
+using System;
 
-namespace Exercicio4
+namespace Exercício3
 {
     class Program
     {
         static void Main(string[] args)
         {
             /*
-             Ex 4 Desenvolva um algoritmo que solicite a entrada da idade de um determinado usuário, 
-             se for menor que 18 anos exiba na cor vermelha “Sem permissão”, 
-             caso seja maior ou igual a 18 anos exiba na cor verde “Permissão concedida”.
-            */
+              Ex 3 Desenvolva uma calculadora, 
+              onde será necessário entrar com a operação, primeiro e segundo valor, 
+              exiba o resultado na tela.
+             */
+            try {
+                Console.WriteLine("Precisamos saber qual é sua idade, para permitir ou não sua entrada:");
 
-            Console.WriteLine("Precisamos saber qual é sua idade, para permitir ou não sua entrada:");
+                var idade = int.Parse(Console.ReadLine());
 
-            var idade = int.Parse(Console.ReadLine());
+                if (idade >= 18)
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Permissão concedida");
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Sem Permissão");
+                }
 
-            if(idade >= 18)
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("Permissão concedida");
+                Console.ReadKey();
+                Console.ReadKey(true);
             }
-            else
+            catch (FormatException)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Sem Permissão");
+                Console.WriteLine("Digite apenas números");
             }
-
-            Console.ReadKey();
         }
     }
 }
